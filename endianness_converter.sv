9f579404d629e3e38a302f868338d188fbb30f9c
Joashy <91395897+Joashy7@users.noreply.github.com>
2026-04-09 18:49:47 +0000

    z

--- endianness_converter.sv
+++ endianness_converter.sv
@@ -1,1 +1,19 @@
 // Put your code below
+module endianness_converter(
+    input reg clk,
+    input int word,
+    input reg endianness_in,
+    input reg endianness_out,
+
+    output int word_out
+);
+
+always @(posedge clk) begin
+    if (endianness_in != endianness_out) begin
+        word_out <= {word[7:0], word[15:8], word[23:16], word[31:24]};
+    end else begin
+        word_out <= word;
+    end
+end
+
+endmodule
